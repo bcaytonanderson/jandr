53d8dc8ccedfa352d8efc04bee3624830aa09474
Jimmy Pocock <jimmypocock@yahoo.com>
2014-10-22 13:30:41 -0500

    Removed Type column from content, updated contents and groups controllers and edit pages to allow for easy editing. Adding some quick links then will most likely move to ajax.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021200057) do
+ActiveRecord::Schema.define(version: 20141022181015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contents", force: true do |t|
-    t.string   "type"
     t.string   "title"
     t.string   "subtitle"
     t.string   "image_key"
