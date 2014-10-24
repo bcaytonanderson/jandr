d40512a1f73a9a2ca14856980bca8946035d180f
Jimmy Pocock <jimmypocock@yahoo.com>
2014-10-24 00:02:07 -0500

    Added a bit of design to the backend.

--- app/assets/javascripts/dashboard_functions.js
+++ app/assets/javascripts/dashboard_functions.js
@@ -1,15 +1,7 @@
 $(document).ready(function(){
-  // setTimeout(function(){
-    // var num;
-    // for(var i = 0; i < groups.length; i++){
-    //   num = groups[i];
-    //   $('.edit-group-'+num+'-form').slideUp();
 
-    //   $('.edit-group-'+num).on('click', function(){
-    //     $('.edit-group-'+num+'-form').slideToggle();
-    //     console.log(num);
-    //   });
-    // }
-  // },1000);
+  $('.submit').on('click',function(){
+    this.blur();
+  });
 
 });
